8b2bbd3927b06875d5d4da531ffdd47656440744
eyal-glick <eyal.golan.gluck@gmail.com>
2025-03-20 10:40:18 +0000

    Eyal Glick manager script

---
+++ folder_task_mgr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+folder_option="a"
+while [[ $folder_option != 0 ]]
+do
+echo "WELCOME TO FOLDER MANAGER"
+echo "========================="
+echo ""     
+echo "Please choose from following options:
+
+     1.) Folder information (files list, extension summary, total size)
+     2.) File reader (contents, counts and search functionality)
+     3.) Folder delete functions (either whole or specific)
+
+For quitting manager, please choose 0."
+read folder_option
+case $folder_option in
+1 )
+    ./utility.sh ;;
+2 )
+    ./read.sh ;;
+3 )
+    ./delete.sh ;;
+0 )
+    exit 0 ;;
+* )
+    echo "Please choose only either one of the numbers 1, 2, 3." ; read ;;
+esac
+clear
+done
